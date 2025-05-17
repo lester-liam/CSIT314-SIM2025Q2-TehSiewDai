04070fad45aac4735e1375d27deb6a9d141387ce
Lester Liam <100615313+lester-liam@users.noreply.github.com>
2025-05-17 18:27:40 +0800

    Updated Repository for Easy Deployment on Render

--- src/entity/Database.php
+++ src/entity/Database.php
@@ -1,21 +1,37 @@
 <?php
 
-class Database
-{
+class Database {
+
     private $db_conn;
 
-    public function __construct()
-    {
-        $this->db_conn = new PDO("mysql:host=db;dbname=csit314", "root", "csit314");
+    public function __construct() {
+
+        // Temporary Certificate File Path
+        $caPath = "../tmp/ca.pem";
+
+        // Write Environment Variable to File (Base64 Decode)
+        if (getenv('DB_SSL_CA_B64')) {
+            file_put_contents($caPath, base64_decode(getenv('DB_SSL_CA_B64')));
+        }
+
+        $uri = getenv('DB_URI');
+        $fields = parse_url($uri);
+        $conn = "mysql:";
+        $conn .= "host=" . $fields["host"];
+        $conn .= ";port=" . $fields["port"];;
+        $conn .= ";dbname=csit314";
+        $conn .= ";sslmode=verify-ca;sslrootcert=../tmp/ca.pem";
+        $this->db_conn = new PDO($conn, $fields["user"], $fields["pass"]);
+
+        // Enable ANSI_QUOTES for this session
+        $this->db_conn->exec("SET SESSION sql_mode = 'ANSI_QUOTES'");
     }
 
-    public function getConnection()
-    {
+    public function getConnection() {
         return $this->db_conn;
     }
 
-    public function __destruct()
-    {
+    public function __destruct() {
         $this->db_conn = null;
     }
 }
