32cfa47dc6c5699f622056572c7b06af2c951265
Lester Liam <100615313+lester-liam@users.noreply.github.com>
2025-05-18 04:08:11 +0800

    Updated viewReport.php from main

--- src/viewReport.php
+++ src/viewReport.php
@@ -50,7 +50,7 @@ if (isset($_GET['reportDate'])) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Service Category</title>
+  <title>Report</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/dropdown.css">
 </head>
@@ -73,7 +73,7 @@ if (isset($_GET['reportDate'])) {
   </div>
 
   <!-- Headline -->
-  <h1>Service Category</h1>
+  <h1>Report</h1>
   <div class="section-container">
     <div class="search">
       <div class="search-group">
